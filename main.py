0f9161538fd21103a0e8de43aa4f02766691a9ed
Shweta Nadge <shwetawsdev@gmail.com>
2026-05-31 18:00:35 +0530

    Added class in main file

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from dotenv import load_dotenv
-# from pydantic import BaseModel
-from groq import Groq
+from pydantic import BaseModel
+# from groq import Groq
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 import os
 
 load_dotenv()
 
-print(os.getenv("YOUR_GROQ_API_KEY"))
-llm = ChatGroq(model="llama-3.3-70b-versatile")
-response = llm.invoke("What is the meaning of life")
-print(response.context)
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+# print(os.getenv("GROQ_API_KEY"))
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("YOUR_GROQ_API_KEY")
+)
+
+# response = llm.invoke("What is the meaning of life")
+# print(response.content)
 
 #set up an LLM
 # llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
